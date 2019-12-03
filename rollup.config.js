83392846a3721e1bddb2e03cd2042372ebeadff7
Yuriy Kravets <yucikkravets@gmail.com>
2019-12-03 14:22:52 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,38 +4,6 @@ import { terser } from 'rollup-plugin-terser';
 import sass from 'node-sass';
 
 const production = !process.env.ROLLUP_WATCH;
-const shared = {
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			preprocess: {
-				style: ({ content, attributes }) => {
-					if (attributes.type !== 'text/scss') return;
-
-					return new Promise((fulfil, reject) => {
-						sass.render({
-							data: content,
-							includePaths: ['src'],
-							sourceMap: true,
-							outFile: 'x' // this is necessary, but is ignored
-						}, (err, result) => {
-							if (err) return reject(err);
-
-							fulfil({
-								code: result.css.toString(),
-								map: result.map.toString()
-							});
-						});
-					});
-				}
-			},
-			customElement: true
-		}),
-		resolve(),
-		production && terser()
-	]
-}
 
 export default [
 	{
